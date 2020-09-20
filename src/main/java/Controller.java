37ba6b391289cb38f3e39f43010412f36825024a
Jgar157 <54415437+Jgar157@users.noreply.github.com>
2020-09-19 21:41:05 -0400

    Database integration into program

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,28 +1,116 @@
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Tab;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
-public class Controller implements Initializable  {
+public class Controller {
 
-  public Tab tab1;
-  //Loading in the fxml
+  /**
+   * Upon the creation of the screen, the combobox is updated and it's becomes editable.
+   */
+  public void initialize() {
 
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
+    //This loop populates the combobox with the numbers 1-10
+    for (int i = 1; i <= 10; i++) {
+
+      cmbbItemType.getItems().add(Integer.toString(i));
+    }
+    //Auto sets the combobox to the first choice
+    cmbbItemType.getSelectionModel().selectFirst();
+    cmbbItemType.setEditable(true);
 
   }
 
-  public void btnAddProductPressed(ActionEvent actionEvent) {
-    System.out.println("Hello World!");
+  //Skeletons for txt boxes and combo boxes
+  @FXML
+  private TextField txtfProductName;
+
+  @FXML
+  private TextField txtfManufacturer;
+
+  @FXML
+  private ComboBox<String> cmbbItemType;
+
+  /**
+   * Method called when the "Add Product Pressed" Button is pressed First, it creates the
+   * connections to the database. Then it sets up variables for connections and statements A
+   * preparedStatement is called to insert values from the text fields, then a statement is used to
+   * output the PRODUCT Table to the console.
+   */
+  public void btnAddProductPressed() {
+
+    final String jdbc_driver = "org.h2.Driver";
+    final String db_url = "jdbc:h2:./res/HR;";
+    /*
+    I had to do some editing to the HR url because it would
+    break whenever I attempted to add something to it.
+    DB_CLOSE_DELAY=-1 Was used to fix this
+     */
+
+    //  Database credentials
+    final String user = "";
+    final String pass = "";
+    Connection conn;
+    PreparedStatement preparedstmt;
+    Statement stmt;
+
+    String tempType = cmbbItemType.getTypeSelector();
+    String tempManufacturer = txtfManufacturer.getText();
+    String tempProductName = txtfProductName.getText();
+
+    try {
+
+      // STEP 1: Register JDBC driver
+      Class.forName(jdbc_driver);
+
+      //STEP 2: Open a connection
+      conn = DriverManager.getConnection(db_url, user, pass);
+
+      //Sql statement
+      String sql = "INSERT INTO PRODUCT(type, manufacturer, name)"
+          + "Values(?, ?, ?)";
+
+      //Puts the corresponding String into the respective ? mark
+      preparedstmt = conn.prepareStatement(sql);
+      preparedstmt.setString(1, tempType);
+      preparedstmt.setString(2, tempManufacturer);
+      preparedstmt.setString(3, tempProductName);
+      preparedstmt.executeUpdate();
+
+      /*
+      Each time a new product is added, the entire list is output
+      to the console.
+      */
+      stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT");
+      while (rs.next()) {
+
+        tempProductName = rs.getString(2); //Grabs col 1,2,3
+        tempType = rs.getString(3);
+        tempManufacturer = rs.getString(4);
+        System.out.println(tempProductName + " " + tempType + " "
+            + tempManufacturer);
+      }
+
+      //Closing statement then connection
+      preparedstmt.close();
+      conn.close();
+
+      //Combined error catching instead of heaving each alone
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+    }
   }
 
-  public void btnRecordProductionPressed(ActionEvent actionEvent) {
-    System.out.println("PRESSED :)))))))))))))))))))))))))))))))))))");
+  /**
+   * Button for the record production button.
+   */
+  public void btnRecordProductionPressed() {
+    System.out.println("Record Production Pressed, pressed :)");
   }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,21 +4,40 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * This is the driver class that runs the program and sets up the screen.
+ * @author Jairo Garciga
+ */
 public class Main extends Application {
 
+  /**
+   * Launches the program.
+   * @param args Command Line arguments.
+   */
   public static void main(String[] args) {
     launch(args);
   }
 
+  /**
+   * start creates the screen according to the saved sizes and also sets up the css.
+   * @param primaryStage The main stage/screen of the program.
+   * @throws Exception Thrown if bad size or css calls.
+   */
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-    Scene scene = new Scene(root, 580, 500);
+    Parent root = FXMLLoader.load(getClass()
+        .getResource("sample.fxml"));
 
-    scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+    int screenWidth = 766;
+    int screenHeight = 574;
 
-    primaryStage.setTitle("FXML Welcome");
+    Scene scene = new Scene(root, screenWidth, screenHeight);
+
+    scene.getStylesheets().add(getClass()
+        .getResource("style.css").toExternalForm());
+
+    primaryStage.setTitle("Production");
     primaryStage.setScene(scene);
     primaryStage.show();
   }
